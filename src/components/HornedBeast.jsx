0f87f5b8b551fb59e1eb7ecf69082d1917843e5d
ned2070te <ned2070te@gmail.com>
2023-10-25 14:59:39 +0100

    gallery working

--- src/components/HornedBeast.jsx
+++ src/components/HornedBeast.jsx
@@ -1,4 +1,9 @@
-
-export default function HornedBeasts(props) {
-  return <div className="hornedBeasts"></div>;
+export default function HornedBeast({ title, imageUrl, description }) {
+  return (
+    <div className="hornedBeast">
+      <h2></h2>
+      <img src={imageUrl} alt="" title={title}></img>
+      <p> {description} </p>
+    </div>
+  );
 }
