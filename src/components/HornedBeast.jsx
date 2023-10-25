e0909db8e670e4d5cc60219b5d52ae64912bda09
ned2070te <ned2070te@gmail.com>
2023-10-25 22:59:40 +0100

    import SeletedBeasts moved from App to HornedBeasts

--- src/components/HornedBeast.jsx
+++ src/components/HornedBeast.jsx
@@ -1,12 +1,29 @@
 import { useState } from "react";
+import SelectedBeast from "./SelectedBeast";
 
 export default function HornedBeast({ title, image_url, description }) {
   // Put likes and setLikes inti useState
   const [likes, setLikes] = useState(0);
+  // start off not showing SelectedBeast
+  const [showSb, setShowSb] = useState(false);
 
   // Click handler
   function handleClick() {
     setLikes(likes + 1);
+    {
+      /* Switches whether SelectedBeast is shown */
+    }
+    {
+      /* If the first value is false the second isn't read */
+    }
+    {
+      showSb && <SelectedBeast handleShowSb={handleShowSb} />;
+    }
+  }
+
+  function handleShowSb() {
+    // flips the Boolean value of showSb
+    setShowSb(!showSb);
   }
 
   // Return element
